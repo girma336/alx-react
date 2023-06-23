5b106b00bf051d1616ec43408978392e21fe4ae3
girma <girmatarekegn.gi@gmail.com>
2023-06-23 09:12:32 +0300

    Add App.js and thire sstyle sheet

--- task/dashboard/src/App.js
+++ task/dashboard/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import './App.css';
+import Notifications from './Notifications';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Login from './components/Login';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <>
+            <Notifications />
+            <div className='app'>
+                <Header />
+                <Login />
+                <Footer />
+            </div>
+        </>
+        
+    )
 }
 
-export default App
+export default App;
